c578d30151e11bdd421c87973e2a9e81483c3e65
StrongAle <alessio.falchi90@gmail.com>
2022-12-15 11:13:39 +0000

    three js performances tests

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import TheCover from './components/TheCover.vue';
+import TheHero from './components/TheHero.vue';
 </script>
 
 <template>
   <div id="wrapper">
     <TheCover />
+    <TheHero />
     <section class="about">
       
     </section>
---
+++ src/components/TheHero.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import TheLogo from './TheLogo.vue';
+</script>
+
+<template>
+<section id="hero">
+        <div class="cover-container">
+            <div class="cover-content">
+                <h1 class="cover-title | heading | text-huge">
+                    <span class="cover-title-first | block ws-nowrap lp-loose">Hi there!</span>
+                    <span class="cover-title-second | block">My name is</span>
+                    <span class="cover-title-third | text-box " data-variant="accent">Alessio</span> 
+                </h1>
+                <p class="cover-subtitle | text-large | lh-loose">
+                Inventive and passionate <strong>Web Developer</strong> since 2014. <br>
+                Storytelling, interactions, a11y and UX oriented.
+                <br>Born in Lucca(IT), London based.
+                    
+                <br>
+                Addicted to music, visual arts, DIY and games.
+                <br>
+                You can call me <strong>Ale</strong>.
+                </p>
+            </div>
+            <div class="cover-logo">
+                
+               
+               <TheLogo></TheLogo>
+                
+            </div>
+        </div>
+    </section>
+</template>
