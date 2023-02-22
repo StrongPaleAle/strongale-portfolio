a0d0766088e22102352da5f7e8a36fa41413ee00
StrongAle <alessio.falchi90@gmail.com>
2023-02-22 19:11:48 +0000

    about components start

---
+++ src/components/BlockText.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {TextBlock} from '@/types';
+
+const props = defineProps<{
+  block: Partial<TextBlock>;
+}>();
+
+</script>
+<template>
+    <div class="content-block" v-html="block.content">
+    </div>
+</template>
