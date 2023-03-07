fdd9b6180d588629e6be2a9ec237fad96aac9c78
StrongAle <alessio.falchi90@gmail.com>
2023-03-07 19:00:43 +0000

    tag and css refactor begin

--- src/components/BlockTags.vue
+++ src/components/BlockTags.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import {DataBlock} from '@/types';
+import {toRefs} from 'vue';
 
 
 const props = defineProps<{
   block: Partial<DataBlock>;
 }>();
+
+const {block} = toRefs(props);
+
+
 </script>
 <template>
     <h4 class="text-accent mt-2em mb-em/2 heading uppercase lh-tight center-line"
         v-if="block.title" >
         <span>{{ block.title }}</span>
     </h4>
-    <div class="flex items-center gap-em/2">
-        <span v-for="item in block.content" class="button tag text-large" :data-value="item.value">
+    <div class="flex justify-center items-center lh-ultra-tight gap-em/2  text-large">
+        <span v-for="item in block.content" class="tag" :style="`--font-size: calc(70% + ${item.value}%);`">
             {{ item.label }}
         </span>
     </div>
