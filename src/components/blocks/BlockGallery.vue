3fc9a69810ec284c4704615dba3cfabaef17c46d
StrongAle <alessio.falchi90@gmail.com>
2023-08-08 19:49:27 +0100

    video controls and begin responsive dialog

--- src/components/blocks/BlockGallery.vue
+++ src/components/blocks/BlockGallery.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 import { Slide } from '@/types';
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
 import BlockPicture from './BlockPicture.vue';
@@ -8,15 +9,19 @@ const props = defineProps<{
     slides: Partial<Slide>[] | undefined; 
     label: string | undefined;
 }>();
-const myOptions = {
+const myOptions : any | undefined = {
   focus    : 'center',
   autoWidth: true,
+  autoHeight: true,
   rewind: true ,
   gap: '0.5rem',
   breakpoints: {
       640: { autoWidth: false, perPage: 1, gap: '0' },
   },
-};
+}; 
+
+
+
 const onMoved = (splide: any, newIndex: number, prevIndex: number, destIndex: number) => {
   console.log('moved', destIndex, newIndex, prevIndex);
   const oldSlide = splide.Components.Slides.getAt(prevIndex);
@@ -29,7 +34,7 @@ const onMoved = (splide: any, newIndex: number, prevIndex: number, destIndex: nu
   if (newVideo) {
     newVideo.play();
   }
-  console.log('oldSlide', oldSlide);
+  //console.log('oldSlide', oldSlide);
 };
 </script>
 <template>
--- src/components/blocks/BlockVideo.vue
+++ src/components/blocks/BlockVideo.vue
@@ -1,18 +1,77 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import type { Ref } from 'vue'
 import { Video } from '@/types';
 
 const props = defineProps<{
     video: Partial<Video>; 
 }>();
+const videoWrapper : Ref<HTMLElement | undefined> = ref();
+const videoOBJ : Ref<HTMLVideoElement | undefined> = ref();
+const videoPlaying = ref(
+  {
+    playing: false,
+    label: 'Play',
+    icon: 'play_circle',
+  }
+);
+
+const playVideo = () => {
+    if (videoOBJ.value) {
+        if (videoOBJ.value.paused) {
+            videoOBJ.value.play();
+            
+        } else {
+            videoOBJ.value.pause();
+        }
+    }
+    console.log(videoPlaying);
+}
+defineExpose({
+  playVideo
+});
+onMounted(() => {
+  //console.log(videoOBJ);
+  videoOBJ.value?.addEventListener('play', () => {
+    videoPlaying.value = {
+      playing: true,
+      label: 'Pause',
+      icon: 'pause_circle',
+    };
+    videoWrapper.value?.classList.add('playing');
+  });
+  videoOBJ.value?.addEventListener('pause', () => {
+    videoPlaying.value = {
+      playing: false,
+      label: 'Play',
+      icon: 'play_circle',
+    };
+    videoWrapper.value?.classList.remove('playing');
+  });
+});
+
 </script>
 <template>
-  <div class="block-picture">
-    <video no-controls loop muted :aria-describedby="`#${ video.id }`">
+  <div class="block-picture" ref="videoWrapper">
+    <video ref="videoOBJ" no-controls loop muted :aria-describedby="`#${ video.id }`">
         <source v-for="source in video.sources" :src="source.src" :type="source.type"/>
         <p>
             Your browser doesn't support HTML video.
         </p>
     </video>
+    <div class="video-controls">
+      <button @click="playVideo" class="playvideo" v-if="videoOBJ">
+        <span class="material-symbols-outlined" aria-hidden="true">{{ videoPlaying.icon }}</span>
+        <span class="visually-hidden">{{ videoPlaying.label }}</span>
+      </button>
+      <span class="indicator">
+        <span class="material-symbols-outlined">
+          no_sound
+          </span>
+        <span class="visually-hidden">No sound</span>
+      </span>
+      
+    </div>
     <p :id="video.id" v-html="video.caption" :class="{'visually-hidden' : !video.showCaption}"></p>
   </div>
 </template>
