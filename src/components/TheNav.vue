84e554cf2404d80d6bcef8d299654ee163cd5714
StrongAle <alessio.falchi90@gmail.com>
2023-08-22 18:20:38 +0100

    options setup

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import TheOptions from "./TheOptions.vue";
+</script>
 <template>
     <nav class="main-nav">
         <div class="main-nav__logo">
@@ -5,22 +9,28 @@
                 <span class="logo-wrapper">
                     <span class="masked-logo"></span>
                 </span>
-                <span class="main-nav__logo-text">Strong<span class="text-accent-color">Ale</span></span>
+                <span class="main-nav__logo-text">
+                    <span>Strong</span>
+                    <strong class="text-accent-color">Ale</strong>
+                </span>
             </a>
         </div>
-        <ul class="main-nav__list">
-            <li class="main-nav__item">
-                <a href="/" class="main-nav__link">Home</a>
-            </li>
-            <li class="main-nav__item">
-                <a href="/#about" class="main-nav__link">About</a>
-            </li>
-            <li class="main-nav__item">
-                <a href="/#portfolio" class="main-nav__link">Work</a>
-            </li>
-            <li class="main-nav__item">
-                <a href="/#contact" class="main-nav__link">Contact</a>
-            </li>
-        </ul>
+        <div class="main-nav__list">
+            <ul class="flex">
+                <li class="main-nav__item">
+                    <a href="/" class="main-nav__link">Home</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/#about" class="main-nav__link">About</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/#portfolio" class="main-nav__link">Work</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="/#contact" class="main-nav__link">Contact</a>
+                </li>
+            </ul>
+            <TheOptions />
+        </div>
     </nav>
 </template>
---
+++ src/components/TheOptions.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="site-options">
+        <fieldset id="theme-group" class="site-options__group">
+            <legend class="visually-hidden">Theme</legend>
+            <input type="radio" name="theme" id="theme-light" value="light" class="sr-only" />
+            <label for="theme-light" class="site-options__label">
+                <span class="visually-hidden">Light theme</span>
+                <span class="material-symbols-outlined" aria-hidden="true">light_mode</span>
+            </label>
+
+        </fieldset>
+    </div>
+</template>
